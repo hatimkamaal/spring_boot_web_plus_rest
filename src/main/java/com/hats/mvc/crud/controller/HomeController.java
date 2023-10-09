e376b148e429c8552aacbd406f679858d434b6f7
hatimkamaal <hatim.kamaal@gmail.com>
2023-10-09 19:10:13 +0530

    Update HomeController.java

--- src/main/java/com/hats/mvc/crud/controller/HomeController.java
+++ src/main/java/com/hats/mvc/crud/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.hats.mvc.crud.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,9 +10,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class HomeController {
 	
+	@Value("${api_uri}")
+	private String api_uri;
+	@Value("${spring.application.name}")
+	String appName;
+	
 	@GetMapping
-	public String homePage() {
+	public String homePage(Model model) {
+		model.addAttribute("appURI", api_uri);
+		model.addAttribute("appName" , appName);
+		System.out.println(api_uri);
 		return "home";
 	}
-	
+
+	@GetMapping("/next")
+	public String nextPage(Model model) {
+		model.addAttribute("appURI", api_uri);
+		model.addAttribute("appName" , appName);
+		System.out.println(api_uri);
+		return "home";
+	}
+
 }
